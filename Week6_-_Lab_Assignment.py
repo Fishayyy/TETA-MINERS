3dad8d8af1cf89bf18fdf35e95272ec8c6941369
unknown <cristiano_aguilar19@hotmail.com>
2021-03-08 02:22:55 -0800

    did the normalize data part just needs to be formatted nicely lol, but IDK if this is how done tho

--- Week6_-_Lab_Assignment.py
+++ Week6_-_Lab_Assignment.py
@@ -46,6 +46,7 @@ reg = LinearRegression(normalize=False)
 reg.fit(X_train, y_train)
 
 reg_pred = reg.predict(X_test)
+print("Linear Regression score: ", reg.score(X_test, y_test)*100)
 
 reg_norm = LinearRegression(normalize=True)
 reg_norm.fit(X_train, y_train)
@@ -95,3 +96,40 @@ print(decision.score(X_test, y_test)*100)
     1)  Repeat part 1 with Normalized data. (Hint: use standarscalar from sklearn)
 '''
 # YOUR CODE GOES HERE  
+from sklearn.preprocessing import StandardScaler
+scalar = StandardScaler()
+scalar.fit(X.data)
+X_norm = scalar.transform(X.data)
+
+#Normalized DATA
+X_train, X_test, y_train, y_test = train_test_split(X_norm, y, test_size=0.2, random_state=RANDOM_SEED)
+
+
+Linear_regression = LinearRegression(normalize=False)
+Linear_regression.fit(X_train, y_train)
+LRpred = Linear_regression.predict(X_test)
+print("Linear Regression with Normalize Data score: ",Linear_regression.score(X_test, y_test)*100)
+
+Linear_regressionNorm = LinearRegression(normalize=True)
+Linear_regressionNorm.fit(X_train, y_train)
+LRNpred = Linear_regressionNorm.predict(X_test)
+print("Linear Regression Norm with Normalize Data score: ",Linear_regressionNorm.score(X_test, y_test)*100)
+
+
+
+
+k = [1, 3, 5, 7, 9, 11, 13, 15]
+neighbors = {}
+for n in k:
+    neigh = KNeighborsRegressor(n_neighbors=n ,p=1)
+    neigh.fit(X_train,y_train)
+    pred_KNRegressor = neigh.predict(X_test)
+    neighbors[n] = pred_KNRegressor
+    print(neigh.score(X_test, y_test)*100)
+
+decision = DecisionTreeRegressor(criterion="mae")
+decision.fit(X_train,y_train)
+pred_KNRegressor = decision.predict(X_test)
+print("\n")
+print(decision.score(X_test, y_test)*100)
+
